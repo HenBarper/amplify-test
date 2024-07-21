61ec88653969d68cc7c687c24674b7e18e7a50aa
Ben Harper <benjaminitonio@gmail.com>
2024-07-21 15:10:16 -0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,33 @@
 import './App.css';
 import { Amplify } from 'aws-amplify';
-import Auth from 'aws-amplify/auth';
-
+import { Authenticator } from '@aws-amplify/ui-react';
+// import awsExports from './aws-exports';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 
+// Amplify.configure(awsExports);
 Amplify.configure(awsconfig);
 
-function App() {
-  const signOut = async () => {
-    try {
-      await Auth.signOut();
-    } catch (error) {
-      console.error(`Error signing out: ${error}`);
-    }
-  }
+export default function App() {
+  // const signOut = async () => {
+  //   try {
+  //     await Auth.signOut();
+  //   } catch (error) {
+  //     console.error(`Error signing out: ${error}`);
+  //   }
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My App</h1>
-        <button onClick={signOut}>Logout</button>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign Out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
